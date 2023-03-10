6cd057c8e41f9a80a285284d563313c84c000cf8
sshabb697 <38155111+sshabb697@users.noreply.github.com>
2023-03-10 16:27:51 +0530

    Update index.php
    updated msyql

--- index.php
+++ index.php
@@ -104,7 +104,7 @@
             <div class="row it_works">
               <?php
 
-                        $link = mysqli_connect('172.20.1.101', 'ecomuser', 'ecompassword', 'ecomdb');
+                        $link = mysqli_connect('shabbir1226-server.mysql.database.azure.com', 'qtnafsxmre', 'B860TA3QZT14Y35B$', 'shabbir1226-database');
 
                         if ($link) {
                         $res = mysqli_query($link, "select * from products;");
